89a5243d3c4adfc92ec138a08cf7f6128951c0db
Kelvy <kelvy0064@gmail.com>
2020-06-28 10:26:45 -0300

    remove empty lines

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,8 @@
 const express = require('express')
-
 const User = require('../models/user')
-
 const bcrypt = require('bcryptjs')
-
 const jwt = require('jsonwebtoken')
-
 const authConfig = require('../src/config/authConfig.json')
-
 const router = express.Router()
 
 function gerenateToken (params = {}) {
@@ -18,38 +13,31 @@ function gerenateToken (params = {}) {
 
 router.post('/register', async (req, res) => {
     const { email } = req.body
-
     try {
         if (await User.findOne({ email })) {
             return res.status(400).send({ error: 'User already exists' })
         }
-
         const user = await User.create(req.body)
-
         user.password = undefined
-
         return res.send({
             user,
             tonken: gerenateToken({ id: user.id })
         })
     } catch (err) {
         return res.status(400).send({ error: 'Registration failed' })
-        }
+    }
 })
 
 router.post('/authenticate', async (req, res) => {
     const { email, password } = req.body
     const user = await User.findOne({ email }).select('+password')
-
     if (!user) {
         return res.status(400).send({ error: 'User not found' })
     }
     if (!await bcrypt.compare(password, user.password)) {
         return res.status(400).send({ error: 'Invalid password' })
     }
-
     user.password = undefined
-
     res.send({
         user,
         tonken: gerenateToken({ id: user.id })
