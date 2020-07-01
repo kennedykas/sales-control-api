094818ed51b6888d476692454809c84e2fa267cf
Kelvy <kelvy0064@gmail.com>
2020-07-01 15:50:09 -0300

    fix: messages changed

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,7 +7,7 @@ const router = express.Router()
 
 function gerenateToken (params = {}) {
     return jwt.sign(params, authConfig.secret, {
-        expiresIn: 86400
+        expiresIn: 36000
     })
 }
 
@@ -15,16 +15,16 @@ router.post('/register', async (req, res) => {
     const { email } = req.body
     try {
         if (await User.findOne({ email })) {
-            return res.status(400).send({ error: 'User already exists' })
+            return res.status(400).send({ error: 'User already exists.' })
         }
         const user = await User.create(req.body)
         user.password = undefined
-        return res.send({
+        return res.status(201).send({
             user,
             tonken: gerenateToken({ id: user.id })
         })
     } catch (err) {
-        return res.status(400).send({ error: 'Registration failed' })
+        return res.status(400).send({ error: 'Registration failed.' })
     }
 })
 
@@ -32,10 +32,10 @@ router.post('/authenticate', async (req, res) => {
     const { email, password } = req.body
     const user = await User.findOne({ email }).select('+password')
     if (!user) {
-        return res.status(400).send({ error: 'User not found' })
+        return res.status(400).send({ error: 'User not found.' })
     }
     if (!await bcrypt.compare(password, user.password)) {
-        return res.status(400).send({ error: 'Invalid password' })
+        return res.status(400).send({ error: 'Invalid password.' })
     }
     user.password = undefined
     res.send({
--- models/user.js
+++ models/user.js
@@ -19,7 +19,6 @@ const UserSchema = new db.Schema({
     },
     email: {
         type: String,
-        unique: true,
         lowercase: true
     },
     password: {
