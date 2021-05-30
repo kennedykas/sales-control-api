a12f4d767c207e0969fc32bb2e1771c2c991acbe
Kelvy <kelvy0064@gmail.com>
2021-05-29 22:35:48 -0300

    fix: ajust mongo url

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
 const db = require('mongoose')
 
-db.connect('mongodb://localhost/noderest', { useNewUrlParser: true, useUnifiedTopology: true })
+db.connect(`${process.env.MONGO_URL}`, { useNewUrlParser: true, useUnifiedTopology: true })
 db.Promise = global.Promise
 
 module.exports = db
